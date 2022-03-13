ee2e189025f230e015d2da7751cef6c0b93f4b66
THEToilet <bp18015@shibaura-it.ac.jp>
2022-03-14 01:38:54 +0900

    :spakles: change branch

--- Assets/Meduse-client/Scripts/Config.cs
+++ Assets/Meduse-client/Scripts/Config.cs
@@ -4,29 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public class ApplicationConfigs : ScriptableObject
+public class Configs : ScriptableObject
 {
-    //例としてPhoto設定,サーバ設定、NCMBの設定を扱う
-    public PhotonConfig PhotonConfig;
-    public ApiConfig ApiConfig;
-    public NcmbConfig NcmbConfig;
+    public ServerConfig ServerConfig;
 }
 
 [Serializable]
-public class ApiConfig
+public class ServerConfig
 {
-    public string BaseUrl = "";
-}
-
-[Serializable]
-public class PhotonConfig
-{
-    public string gameVersion = "";
-}
-
-[Serializable]
-public class NcmbConfig
-{
-    public string ApplicationKey = "";
-    public string ClientKey = "";
+    public string Url = "";
+    public string Port = "";
 }
